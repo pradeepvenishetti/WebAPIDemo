cc864e674ec14cba5cfa12cf67f71ab704f11b38
pradeep <pradeep@PRADEEP>
2016-12-04 15:43:19 -0600

    Add project files.

---
+++ WebAPIDemo/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    public class StudentController : ApiController
+    {
+
+        private StudentRepository studentRepository;
+
+        public StudentController()
+        {
+            this.studentRepository = new StudentRepository();
+        }
+        public Student[] GetStudents()
+        {
+            return studentRepository.GetAllStudents();
+        }
+
+        public HttpResponseMessage Post(Student student)
+        {
+            this.studentRepository.SaveStudent(student);
+
+            var response = Request.CreateResponse<Student>(System.Net.HttpStatusCode.Created, student);
+
+            return response;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            var student = studentRepository.GetAllStudents().FirstOrDefault((s) => s.Id == id);
+            if (student == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return student;
+        }
+    }
+}
---
+++ WebAPIDemo/Models/StudentRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo.Models
+{
+    public class StudentRepository
+    {
+        private const string CacheKey = "StudentStore";
+
+
+        public StudentRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Student[]
+                    {
+                new Student
+                {
+                    Id = 1, Name = "Glenn Block"
+                },
+                new Student
+                {
+                    Id = 2, Name = "Dan Roth"
+                }
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
+        public Student[] GetAllStudents()
+        {
+
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Student[])ctx.Cache[CacheKey];
+            }
+
+            return new Student[]
+                {
+            new Student
+            {
+                Id = 0,
+                Name = "Placeholder"
+            }
+                };
+
+        }
+
+
+        public bool SaveStudent(Student student)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Student[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(student);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
